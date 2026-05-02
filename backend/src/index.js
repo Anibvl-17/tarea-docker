fbf3e146f6d16df1cefeeb5df9e9b6866d6f3c87
Anibvl-17 <claudio.candia2301@alumnos.ubiobio.cl>
2026-05-02 18:24:21 -0400

    feat: app basica express

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,13 @@
-console.log("hola")
+import express from 'express';
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+})
+
+app.listen(PORT, () => {
+  console.log(`=> Servidor corriendo en http://localhost:${PORT}`);
+})
